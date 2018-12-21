63baf839e7877030775915f33e395a549406d664
Chris <christian.macias@cceo.com.mx>
2018-12-21 16:00:12 -0600

    intdouble

--- bin/strings.dart
+++ bin/strings.dart
@@ -1,4 +1,16 @@
 main(List<String> arguments) {
   var country = "Mexico";
+  String name = "Chris";
+
+  country = "Japan";
+
+  int age = 23;
+  double num  = 12.34;
+
+
+
   print(country);
+  print(name);
+  print(age);
+  print(num);
 }
